7041a363b7280922dfdef2bc41ffbac9ed2b841b
TeukuNeo <155743925+TeukuNeo@users.noreply.github.com>
2024-05-20 13:15:29 +0700

    Add files via upload

---
+++ MARI COBA SESUATU/sumber/data.js
@@ -0,0 +1,100 @@
+
+let shopItemsData = [{
+    id: "MLO",
+    name: "Milo",
+    price: 4,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/MILO.jpg"
+},{
+    id: "AMO",
+    name: "AMERICANO",
+    price: 3,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/AMERICANO.jpg"
+},{
+    id: "CPO",
+    name: "CAPPUCINO",
+    price: 5,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/CAPPUCINO.jpg"
+},{
+    id: "LTE",
+    name: "LATTE",
+    price: 4,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/LATTE.jpg"
+},{
+    id: "PCK",
+    name: "PANCAKE",
+    price: 7,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/PANCAKE.jpg"
+},{
+    id: "CRS",
+    name: "CROISANT",
+    price: 5,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/CROISANT.jpg"
+},{
+    id: "WFL",
+    name: "WAFFLE",
+    price: 8,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/WAFFLE.jpg"
+},{
+    id: "SWC",
+    name: "SANDWICH",
+    price: 9,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/SANDWICH.jpg"
+}]
+
+let productItemsData = [{
+    id: "KOS",
+    name: "Kaos",
+    price: 11,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/need-more-coffee.jpg"
+}, {
+    id: "BTL",
+    name: "Botol",
+    price: 13,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/need-more-coffee.jpg"
+}, {
+    id: "MUG",
+    name: "Mug",
+    price: 12,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/need-more-coffee.jpg"
+}, {
+    id: "PLT",
+    name: "Topi",
+    price: 10,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/need-more-coffee.jpg"
+}, {
+    id: "TSO",
+    name: "Americano",
+    price: 18,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/jason-rojas.jpg"
+}, {
+    id: "MLT",
+    name: "Brazilian",
+    price: 18,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/jason-rojas.jpg"
+}, {
+    id: "DGU",
+    name: "Arabica",
+    price: 18,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/jason-rojas.jpg"
+}, {
+    id: "QTL",
+    name: "Espresso",
+    price: 18,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+    img: "gambar/jason-rojas.jpg"
+}]
